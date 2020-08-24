9e3a9f80e7f61e1a50d59e882aeb758c733c7960
rssarto <ricardo.soares.sarto@gmail.com>
2020-08-23 22:53:50 -0300

    Api documentation

---
+++ ab-inbev_app/src/main/java/com/abinbev/config/swagger/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.abinbev.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private static final Contact DEFAULT_CONTACT = new Contact("Ricardo Sarto", "https://rssarto.github.io/resume/", "ricardo.soares.sarto@gmail.com");
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("AB-Inbev Code Challenge", "AB-Inbev Code Challenge API documentation", "1.0", "urn:tos",
+            DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/json"));
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+    }
+}
--- ab-inbev_app/src/main/java/com/abinbev/controllers/ProductController.java
+++ ab-inbev_app/src/main/java/com/abinbev/controllers/ProductController.java
@@ -2,13 +2,14 @@ package com.abinbev.controllers;
 
 import com.abinbev.model.Product;
 import com.abinbev.services.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RequestMapping(value = ProductController.URI_PREFIX)
@@ -22,36 +23,42 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @ApiOperation(value = "Creates a new Product")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void create(@RequestBody final Product product){
+    public void create(@ApiParam(value = "Product definition") @RequestBody final Product product){
         this.productService.create(product);
     }
 
+    @ApiOperation(value = "Deletes a Product by Id")
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{productId}")
-    public void delete(@PathVariable("productId") final UUID productId){
+    public void delete(@ApiParam(value="Product Id") @PathVariable("productId") final UUID productId){
         this.productService.delete(productId);
     }
 
+    @ApiOperation(value = "Updates a Product")
     @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{productId}")
-    public void update(@PathVariable("productId") final UUID productId, @RequestBody final Product product){
+    public void update(@ApiParam(value="Product Id") @PathVariable("productId") final UUID productId, @ApiParam(value = "Product definition") @RequestBody final Product product){
         this.productService.save(productId, product);
     }
 
+    @ApiOperation(value = "Find Products by name containing value")
     @GetMapping("/name/{name}")
-    public ResponseEntity<Page<Product>> findByName(@PathVariable("name") final String name, Pageable pageable){
+    public ResponseEntity<Page<Product>> findByName(@ApiParam(value="Name filter") @PathVariable("name") final String name, @ApiParam(value="Page definition") Pageable pageable){
         return new ResponseEntity<>(this.productService.findByName(name, pageable), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Find Product by Id")
     @GetMapping("/{productId}")
-    public ResponseEntity<Product> findById(@PathVariable("productId") final UUID productId){
+    public ResponseEntity<Product> findById(@ApiParam(value="Product Id") @PathVariable("productId") final UUID productId){
         return new ResponseEntity<>(this.productService.findById(productId), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Find all products")
     @GetMapping
-    public ResponseEntity<Page<Product>> findAll(Pageable pageable){
+    public ResponseEntity<Page<Product>> findAll(@ApiParam(value="Page definition") Pageable pageable){
         return new ResponseEntity<>(this.productService.findAll(pageable), HttpStatus.OK);
     }
 
